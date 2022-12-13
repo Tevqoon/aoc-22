b2b87edc0fd5d4624dfa8a9f6c692c69678ab9ce
Jure Smolar <jure@Second-Brain2.home>
2022-12-13 10:46:14 +0100

    13

---
+++ codes/13.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(require "common.rkt")
+
+(define input (filter-map (compose (λ(x) (if (equal? eof x) #f x))
+                                   (curryr call-with-input-string read)
+                                   (curryr string-replace "," " "))
+                          (file->lines "../inputs/13.txt")))
+
+
+(define (check? a b)
+  (match (list a b)
+    [(list a b) #:when (andmap number? `(,a ,b))
+                (if (= a b)
+                    'no
+                    (< a b))]
+    [(list '() '()) 'no]
+    [(list '() (cons b bs)) #t]
+    [(list (cons a as) '()) #f]
+    [(list (cons a as) (cons b bs)) (let ([checked (check? a b)])
+                                      (if (equal? checked 'no)
+                                          (check? as bs)
+                                          checked))]
+    [(list a b) #:when (list? b) (check? (list a) b)]
+    [(list a b) #:when (list? a) (check? a (list b))]
+    ))
+
+(apply + (map add1 (indexes-where (slice-up input 2) (curry apply check?))))
+
+(let* ([sorted (sort (append input '(((2)) ((6)))) check?)]
+       [index2 (+ 1 (index-of sorted '((2))))]
+       [index6 (+ 1 (index-of sorted '((6))))])
+  (* index2 index6))
