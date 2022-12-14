7a3d5e0de62261de0d740a7e808974de490a7e7d
Jure Smolar <jure@Second-Brain2.home>
2022-12-15 00:31:30 +0100

    meh 14

--- codes/14.py
+++ codes/14.py
@@ -2,14 +2,8 @@ from math import copysign
 
 with open("../inputs/14.txt") as f:
     lines = [[tuple(map(int,pair.split(","))) for pair in line.rstrip().split(" -> ")] for line in f]
-
-sample = [[(498,4), (498,6), (496,6)], [(503,4), (502,4), (502,9), (494,9)]]
-
-sand1 = dict()
-sand2 = dict()
-
-def addt(t1, t2):
-    return tuple(map(sum, zip(t1, t2)))
+    
+sand = set()
 
 def draw(p1, p2):
     "Returns all the points between two points (assumed hor/vert line)"
@@ -27,45 +21,56 @@ for line in lines:
     for first, second in zip(line, line[1:]):
         for point in draw(first, second):
             maxd = max(maxd, point[1])
-            sand1[point] = "#"
-            sand2[point] = "#"
+            sand.add(point)
+maxd += 1
+            
+def drop1():
+    p1, p2 = 500, 0
+    while p2 < maxd:
+        if not((p1, p2 + 1) in sand):
+            p2 += 1
+        elif not((p1 - 1, p2 + 1) in sand):
+            p1 -= 1
+            p2 += 1
+        elif not((p1 + 1, p2 + 1) in sand):
+            p1 += 1
+            p2 += 1
+        else:
+            sand.add((p1, p2))
+            return True
+    return False
 
-def drop1(point=(500,0)):
-    if point[1] > 500:
-        return False
-    elif not(addt(point, (0, 1)) in sand1):
-        return drop1(addt(point, (0, 1)))
-    elif not(addt(point, (-1, 1)) in sand1):
-        return drop1(addt(point, (-1, 1)))
-    elif not(addt(point, (1, 1)) in sand1):
-        return drop1(addt(point, (1,1)))
-    else:
-        sand1[point] = "o"
-        return True
+def drop2():
+    p1, p2 = 500, 0
+    while not((500, 0) in sand):
+        if p2 == maxd:
+            sand.add((p1, p2))
+            return True
+        elif not((p1, p2 + 1) in sand):
+            p2 += 1
+        elif not((p1 - 1, p2 + 1) in sand):
+            p1 -= 1
+            p2 += 1
+        elif not((p1 + 1, p2 + 1) in sand):
+            p1 += 1
+            p2 += 1
+        else:
+            sand.add((p1, p2))
+            return True
+    return False
 
-def drop2(point=(500,0)):
-    if (500, 0) in sand2:
-        return False
-    elif point[1] == maxd + 1:
-        sand2[point] = "o"
-        return True
-    elif not(addt(point, (0, 1)) in sand2):
-        return drop2(addt(point, (0, 1)))
-    elif not(addt(point, (-1, 1)) in sand2):
-        return drop2(addt(point, (-1, 1)))
-    elif not(addt(point, (1, 1)) in sand2):
-        return drop2(addt(point, (1,1)))
-    else:
-        sand2[point] = "o"
-        return True
-    
+s = 0
 while drop1():
+    s += 1
     pass
 
-while drop2():
+print(s)
+
+# The first sort of drop will always fill up first.
+while drop2(): 
+    s += 1
     pass
 
-print(list(sand1.values()).count("o"))
-print(list(sand2.values()).count("o"))
+print(s)
         
 
