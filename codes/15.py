c15bdb63482a4da724e73dd34bf8c784c3ecaf01
Jure Smolar <jure@Second-Brain2.home>
2022-12-17 02:17:27 +0100

    15 yassification

--- codes/15.py
+++ codes/15.py
@@ -1,62 +1,34 @@
-from collections import defaultdict
-
-def manhattan(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return abs(x1 - x2) + abs(y1 - y2)
-
-def process(line):
-    lst = [int(x.replace(",", "").replace(":", "").split("=")[1]) for x in line.rstrip().split(" ") if x != x.split("=")[0]]
-    x1, y1, x2, y2 = tuple(lst)
-    return (x1, y1, x2, y2, manhattan((x1, y1), (x2, y2)))
+from tqdm import tqdm
+import re
 
 with open("../inputs/15.txt") as f:
-    lines = [process(line) for line in f]
+    lines = [[int(x) for x in re.findall(r'-?\d+', line)] for line in f]
 
-sensors = [(x1, y1, d) for x1, y1, _, _, d in lines]
-beacons = {(x2, y2)    for _, _, x2, y2, _ in lines}
+sensors = sorted([(abs(x1 - x2) + abs(y1 - y2), x1, y1) for x1, y1, x2, y2 in lines], reverse=1)
 
-def merge_intervals(i1, i2):
-    a, b = i1
-    c, d = i2
-    if a - 1 <= c <= b + 1 or c - 1 <= a <= d + 1:
-        return [(min(a, c), max(b,d))]
-    elif b < c:
-        return [(a, b), (c, d)]
-    elif d < a:
-        return [(c, d), (a, b)]
+merge = lambda a,b,c,d : (None, (min(a, c), max(b, d))) if a - 1 <= c <= b + 1 else ((a, b), (c, d))
 
-def add_interval(intervals, new_interval):
-    if intervals == []:
-        return [new_interval]
-    new = merge_intervals(intervals[0], new_interval)
-    if len(intervals) == 1:
-        return new
-    else:
-        return new[:-1] + add_interval(intervals[1:], new[-1])
-
+def add_interval(intervals, new_i, index=0):
+    if new_i[0] > new_i[1]: return
+    while index < len(intervals):
+        i1, new_i = merge(*sum(sorted([intervals.pop(index), new_i]), ()))
+        if i1: index, _ = index + 1, intervals.insert(index, i1)
+    intervals.append(new_i)
+            
 # Part 1
 rowy = 2000000
 firstint = []
-for x1, y1, d in sensors:
-    lr = d - abs(y1 - rowy) # if < 0, we have nothing left
-    if lr > 0:
-        firstint = add_interval(firstint, (x1 - lr, x1 + lr))
+for d, x1, y1 in sensors:
+    add_interval(firstint, (x1 - (lr := d - abs(y1 - rowy)), x1 + lr))
 
-print(sum([y - x for x, y in firstint]))
+print(sum([y - x + 1 for x, y in firstint]) - len({1 for _, _, _, y in lines if y == rowy}))
   
-        
 # Part 2
-dmax = 4000000        
-rows = defaultdict(lambda: [])
-for x1, y1, d in sensors:
-    for y in range(max(y1 - d, 0), min(y1 + d + 1, dmax)):
-        lr = abs(d - abs(y1 - y))
-        rows[y] = add_interval(rows[y], (max(x1 - lr, 0), min(x1 + lr, dmax)))
-
-for k, v in rows.items():
-    if len(v) > 1:
-        print((v[0][1] + 1) * 4000000 + k)
+dmax = 4000000
+for x, y in (([], y) for y in tqdm(range(dmax))):
+    for d, x1, y1 in sensors:
+        add_interval(x, (max(x1 - (lr := d - abs(y1 - y)), 0), min(x1 + lr, dmax)))
+        if x == [(0, dmax)]: break
+    else:
+        print((x[0][1] + 1) * dmax + y)
         break
-
-
