3f96e88f5ab3e88121df7e2e8c5b5434c4db5e0a
Jure Smolar <jure@Second-Brain2.home>
2022-12-23 23:39:17 +0100

    21

--- codes/18.py
+++ codes/18.py
@@ -1,12 +1,7 @@
-from itertools import product
-
 with open("../inputs/18.txt") as f:
     lines = {tuple(map(lambda x : int(x), line.split(","))) for line in f}
 
-maxx = max(lines, key=lambda x: x[0])[0]
-maxy = max(lines, key=lambda x: x[1])[1]
-maxz = max(lines, key=lambda x: x[2])[2]
-
+maxx, maxy, maxz = [max(lines, key=lambda x: x[i])[i] for i in range(3)]
 dirs = [(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]
 
 def area(working):
@@ -14,8 +9,7 @@ def area(working):
     for x,y,z in working:
         tmp = 6
         for dx, dy, dz in dirs:
-            if (x + dx, y + dy, z + dz) in working:
-                tmp -= 1
+            tmp -= 1 if (x + dx, y + dy, z + dz) in working else 0
         count += tmp
     return count
 
@@ -26,12 +20,13 @@ table = [[[(x,y,z) in lines for z in range(maxz + 1)] for y in range(maxy + 1)] 
 Q = [(0,0,0)]
 while Q:
     x,y,z = Q.pop()
-    if 0 <= x <= maxx and 0 <= y <= maxy and 0 <= z <= maxz and not table[x][y][z]:
-        table[x][y][z] = True
-        for dx, dy, dz in dirs:
-            if (0 <= x + dx <= maxx and 0 <= y + dy <= maxy and 0 <= z + dz <= maxz and
-                not table[x + dx][y + dy][z + dz]):
-                Q.append((x + dx, y + dy, z + dz))
+    for dx, dy, dz in dirs:
+        if (0 <= (nx := x + dx) <= maxx and
+            0 <= (ny := y + dy) <= maxy and
+            0 <= (nz := z + dz) <= maxz and
+            not table[nx][ny][nz]):
+            table[nx][ny][nz] = True
+            Q.append((nx, ny, nz))
 
 pockets = set()
 for i, x in enumerate(table):
@@ -41,3 +36,4 @@ for i, x in enumerate(table):
                 pockets.add((i,j,k))
 
 print(count_all - area(pockets))
+
---
+++ codes/21.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+(require "common.rkt")
+
+(define input (map (λ(line) (match (string-split (string-replace line ":" "") " ")
+                              [(list name num) (cons name (string->number num))]
+                              [(list name mon1 op mon2)
+                               (cons name (list (eval (call-with-input-string op read))
+                                                mon1 mon2))]))
+                   (file->lines "../inputs/21.txt")))
+
+(define (op-1 op)
+  ((λ(x) (if x (cdr x) op)) (assoc op `((,+ . ,-) (,- . ,+) (,* . ,/) (,/ . ,*)))))
+
+(define (solver1 inp [cell "root"])
+  (match (cdr (assoc cell inp))
+    [(list op a1 a2) (op (solver1 inp a1) (solver1 inp a2))]
+    [a a]))
+
+(writeln (solver1 input))
+
+(define (solver2 inp [cell "root"])
+  (match (cdr (assoc cell inp))
+    [(list op a b)
+     (let-values ([(left fl)  (solver2 inp a)]
+                  [(right fr) (solver2 inp b)]
+                  [(op) (if (equal? cell "root") (λ(x f) f) op)])
+       (cond [fl (values left (λ(y) (fl ((op-1 op) y right))))]
+             [(and fr (member op (list - /)))
+              (values right (λ(y) (fr (op left y))))]
+             [fr (values right (λ(y) (fr ((op-1 op) y left))))]
+             [else (values (op left right) #f)]))]
+    [a (if (equal? cell "humn") (values 'x identity) (values a #f))]))
+
+(writeln (call-with-values (λ() (solver2 input)) (λ(x f) (f 0))))
